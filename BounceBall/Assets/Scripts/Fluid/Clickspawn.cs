af64573f34049b3a1ff8ac88daf16abff56569ff
makehczhu <1364981631@qq.com>
2019-07-20 16:26:41 +0800

    add effect fluid

---
+++ BounceBall/Assets/Scripts/Fluid/Clickspawn.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Clickspawn : MonoBehaviour {
+
+    public GameObject Brick;
+	
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject brick = Instantiate(Brick, Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0,0,-Camera.main.transform.position.z)), Brick.transform.rotation) as GameObject;
+            brick.transform.Rotate(0, 0, UnityEngine.Random.Range(0, 0));
+            brick.GetComponent<Rigidbody2D>().mass = brick.transform.localScale.x * brick.transform.localScale.y/2;
+            Destroy(brick, 1.4f);
+        }
+	}
+}
---
+++ BounceBall/Assets/Scripts/Fluid/WaterDetector.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterDetector : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.GetComponent<Rigidbody2D>() != null)
+        {
+            transform.parent.GetComponent<Fluid>().Splash(transform.position.x, collision.GetComponent<Rigidbody2D>().velocity.y * collision.GetComponent<Rigidbody2D>().mass / 40.0f);
+        }
+    }
+}
